c8cbadc2c3dfaaa1296f61340f3f336c65a655b9
Sarunas Motiejunas <motiejunas.sarunas@gmail.com>
2017-02-07 21:40:51 +0200

    Trying to get angular 2 working, so far no good

---
+++ app/app.login.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/pltaform-browser';
+import { AppComponent } from 'app.login';
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
