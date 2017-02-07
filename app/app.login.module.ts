feca00be6ebf24fc22763293a42004afe5564560
Sarunas Motiejunas <motiejunas.sarunas@gmail.com>
2017-02-07 21:58:04 +0200

    Angular 2 finally working, managed to squeeze out little hello from it

--- app/app.login.module.ts
+++ app/app.login.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/pltaform-browser';
-import { AppComponent } from 'app.login';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.login';
 
 @NgModule({
   imports: [ BrowserModule ],
