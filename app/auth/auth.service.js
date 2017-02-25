7f8ec56bf0b8ebc643f86661c655ecab863bd783
Sarunas Motiejunas <motiejunas.sarunas@gmail.com>
2017-02-25 22:26:15 +0200

    It's bad

--- app/auth/auth.service.js
+++ app/auth/auth.service.js
@@ -5,21 +5,46 @@ var http = require('http');
 var xml = require('fs').readFileSync('auth.wsdl', 'utf8');
 
 var service = {
-    loginService : {
-        loginPort : {
+    MyPromiseService : {
+        promisePort : {
             memorize: function (args) {
-                return {
-                    formatedText: args.email + " " + args.password
+                if (args.email == "test@gmail.com" && args.password == "slaptazodis"){
+                  return {
+                    formatedText: "You are signed in using email: " + args.email + " using password: " + args.password
+                }
+                }
+                else {
+                  return {
+                    formatedText: "Wrong username or password"
+                  }
                 }
             }
+
+/*            MyAsyncFunction: function(args, callback){
+              callback({
+                email: args.email
+              });
+            },
+
+            HeadersAwareFunction: function(args, cb, headers) {
+              return {
+                email: headers.Token
+              };
+            },
+
+            reallyDetailedFunction: function(args, cb, headers, req){
+              console.log('SOAP `reallyDetailedFunction` request from ' + req.connection.remoteAddress);
+              return{
+                email: headers.Token
+              };*/
+            }
         }
-    }
-};
+    };
 
 
 var server = http.createServer(function(request,response) {
     response.end("404: Not Found: " + request.url);
 });
 
-server.listen(3000);
+server.listen(8001);
 soap.listen(server, '/auth', service, xml);
